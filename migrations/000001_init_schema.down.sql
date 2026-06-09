417de5b40f65b2a832143961bf312b556895a6c9
miracle <miraclechukwu2011@gmail.com>
2026-06-09 23:33:20 +0100

    updated migration init schema down sql

--- migrations/000001_init_schema.down.sql
+++ migrations/000001_init_schema.down.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS audit_logs;
 DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS user_oauth_providers;
 DROP TABLE IF EXISTS users;
 DROP FUNCTION IF EXISTS update_updated_at_column;
